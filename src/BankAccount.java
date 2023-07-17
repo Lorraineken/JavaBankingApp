6630f915d32afd4fab5f53eecaa9ffdcb7cf604d
Lorraine Kupa <lorrainekupa@gmail.com>
2023-07-17 15:38:59 +0300

    Added Constructor and Getters for BankAccount class

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,19 @@
 public class BankAccount {
     private String owner;
     private double balance;
+
+    // constructor
+    BankAccount(String owner,double balance){
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    //Getters
+    public String getOwner() {
+        return owner;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
