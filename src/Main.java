01e2f8983c0efe35eed06a60a208ce3aa7de7aa3
Lorraine Kupa <lorrainekupa@gmail.com>
2023-07-17 15:57:55 +0300

    Created a new bank Account instance

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-
+      // Bank Account Instance
+        BankAccount myAccount = new BankAccount("Lorraine",2000);
+        myAccount.withdraw(500);
+        System.out.println(myAccount.getBalance());
     }
 }
